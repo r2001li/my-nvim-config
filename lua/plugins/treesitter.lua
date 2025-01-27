432b253fd9af1d4f71c235233f615d49b5648d84
RLi <ruiang2001@gmail.com>
2025-01-27 17:42:44 -0500

    Ensured treesitter isntalls parsers for some languages

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -6,6 +6,7 @@ return {
 		local config = require("nvim-treesitter.configs")
 		config.setup({
 			auto_install = true,
+			ensure_installed = { "lua", "bash", "cpp", "c" },
 			highlight = { enable = true },
 		})
 	end,
