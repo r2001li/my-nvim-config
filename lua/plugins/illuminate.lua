f7f72e6f9c426bc12a7a8e2adcccbe599ae8e620
RLi <ruiang2001@gmail.com>
2025-01-27 17:35:38 -0500

    Codeium suggestions now in completion snippets

--- lua/plugins/illuminate.lua
+++ lua/plugins/illuminate.lua
@@ -1,6 +1,14 @@
 return {
 	"RRethy/vim-illuminate",
 	config = function()
-		require("illuminate").configure()
+		require("illuminate").configure({
+			providers = {
+				"lsp",
+				"treesitter",
+				"regex",
+			},
+			delay = 100,
+			under_cursor = true,
+		})
 	end,
 }
--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,30 +1,32 @@
 return {
 	{
 		"williamboman/mason.nvim",
-
+		lazy = false,
 		config = function()
 			require("mason").setup()
 		end,
 	},
 	{
 		"williamboman/mason-lspconfig.nvim",
-
-		config = function()
-			require("mason-lspconfig").setup({
-				ensure_installed = {
-					"lua_ls",
-					"clangd",
-				},
-			})
-		end,
+		lazy = false,
+		opts = {
+			auto_install = true,
+		}
 	},
 	{
 		"neovim/nvim-lspconfig",
-
+		lazy = false,
 		config = function()
+			local capabilities = require('cmp_nvim_lsp').default_capabilities()
 			local lspconfig = require("lspconfig")
-			lspconfig.lua_ls.setup({})
-			lspconfig.clangd.setup({})
+
+			lspconfig.lua_ls.setup({
+				capabilities = capabilities
+			})
+			lspconfig.clangd.setup({
+				capabilities = capabilities
+			})
+
 			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
 			vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
 			vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
