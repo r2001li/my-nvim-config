d415611b7869f0bab74639d6d8e7e47b435055f7
RLi <ruiang2001@gmail.com>
2025-01-26 20:33:56 -0500

    Added lsp

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,5 +1,28 @@
 return {
 	"nvim-treesitter/nvim-treesitter",
+	
+	opts = {
+		ensure_installed = {
+			"bash",
+			"c",
+			"cpp",
+			"css",
+			"html",
+			"javascript",
+			"json",
+			"lua",
+			"markdown",
+			"markdown_inline",
+			"python",
+			"rust",
+			"toml",
+			"tsx",
+			"typescript",
+			"vim",
+			"yaml",
+		},
+	}, 
+
 	build = function()
 		require("nvim-treesitter.install").update({ with_sync = true })()
 	end,
